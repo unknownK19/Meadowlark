55b2980af1a9bbc7060051fb3ae6586c07c03100
K19 <107351441+unknownK19@users.noreply.github.com>
2024-03-07 12:06:33 +0530

    Update style.rs

--- src/style.rs
+++ src/style.rs
@@ -1,12 +1,15 @@
-// TODO Customize Layout
+// TODO Customize More Layout
 
 use iced::{
+    border::Radius,
     theme::Palette,
     widget::button::{self, Appearance},
     Background, Border, Color, Theme,
 };
 
-pub struct DefaultButton(pub Palette);
+use crate::Message;
+
+pub struct DefaultButton(pub Palette, pub Message);
 
 impl button::StyleSheet for DefaultButton {
     type Style = Theme;
@@ -15,27 +18,36 @@ impl button::StyleSheet for DefaultButton {
             background: Some(Background::Color(Color::from_rgb(
                 self.0.primary.r,
                 self.0.primary.g,
-                self.0.primary.b, //0.6, 0.6, 0.6,
+                self.0.primary.b,
             ))),
-            border: Border::with_radius(5),
+            border: Border::with_radius(10),
             ..Default::default()
         }
     }
     fn hovered(&self, style: &Self::Style) -> Appearance {
+        Message::Clickaction(2);
+        println!("Hovered me");
         button::Appearance {
-            border: Border::with_radius(8),
+            background: Some(Background::Color(Color {
+                a: 1.0,
+                ..self.0.background
+            })),
+            border: Border {
+                color: self.0.primary,
+                width: 2.0,
+                radius: Radius::from(10.0),
+            },
+            text_color: Color { ..self.0.primary },
             ..self.active(style)
         }
     }
     fn disabled(&self, style: &Self::Style) -> Appearance {
         button::Appearance {
-            border: Border::with_radius(8),
             ..self.active(style)
         }
     }
     fn pressed(&self, style: &Self::Style) -> Appearance {
         button::Appearance {
-            border: Border::with_radius(8),
             ..self.active(style)
         }
     }
